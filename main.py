ed9080fffa6c3ead3f7201a37c9645c92925e084
Ubsilver <kraska228188@gmail.com>
2024-04-06 14:46:00 +0500

    init

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import cv2
 import numpy as np
 
-my_photo = cv2.imread('C:/Users/asonza/Downloads/dataroad1.png')
+# Загрузка изображения
+my_photo = cv2.imread('C:/Users/asonza/Downloads/dataroad2.png')
 
 # Преобразование изображения в черно-белое
 bw_image = cv2.cvtColor(my_photo, cv2.COLOR_BGR2GRAY)
 
-# Применение медианного фильтра с параметром 10
-median_image = cv2.medianBlur(bw_image, 5)
+# Применение уравнивания гистограммы для увеличения контрастности
+kernel = np.array([[-1,0,1], [-2,0,2], [-1,0,1]])
+equalized_image = cv2.filter2D(bw_image, -1, kernel)
+
+# Применение медианного фильтра с параметром 5
+median_image = cv2.medianBlur(equalized_image, 5)
+
 # Применение бинаризации для выделения контуров
-_, binary_image = cv2.threshold(median_image, 0, 255, cv2.THRESH_BINARY)
+_, binary_image = cv2.threshold(median_image, 20, 255, cv2.THRESH_BINARY)
 
 # Нахождение контуров
 contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -18,11 +24,12 @@ contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX
 contour_image = np.zeros_like(my_photo)
 
 # Нарисовать контуры на пустом изображении
-cv2.drawContours(contour_image, contours, -1, (255, 255, 255), 2)
+cv2.drawContours(contour_image, contours, -1, (255, 255, 255), 1)
 
-# Отображение исходного, медианно отфильтрованного и контурного изображений
+# Отображение исходного, уравненного, медианно отфильтрованного и контурного изображений
 cv2.imshow('Original Image', my_photo)
-cv2.imshow('Median Filtered Image', median_image)
+cv2.imshow('Equalized Image', equalized_image)
+# cv2.imshow('Median Filtered Image', median_image)
 cv2.imshow('Contours', contour_image)
 
 # Ожидание нажатия клавиши для закрытия окон
